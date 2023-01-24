c89e604f96bacc113697f48b54a02d6bcc0e4dd4
jgy <duke407@naver.com>
2023-01-24 20:27:42 +0900

    update

--- src/main/java/com/poyo/springboot/domain/posts/Posts.java
+++ src/main/java/com/poyo/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.poyo.springboot.domain.posts;
 
+import com.poyo.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +16,7 @@ import javax.persistence.Id;
 @Entity
 // 테이블과 링크될 클래스임을 나타냄
 // 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍(_)으로 테이블 이름을 매칭
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id // 해당 테이블의 PK필드를 나타냄
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,6 +39,11 @@ public class Posts {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title,String content) {
+        this.title = title;
+        this.content = content;
     }
 
 }
---
+++ src/main/java/com/poyo/springboot/service/PostsService.java
@@ -0,0 +1,37 @@
+package com.poyo.springboot.service;
+
+import com.poyo.springboot.domain.posts.Posts;
+import com.poyo.springboot.domain.posts.PostsRepository;
+import com.poyo.springboot.web.dto.PostsResponseDto;
+import com.poyo.springboot.web.dto.PostsSaveRequestDto;
+import com.poyo.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
