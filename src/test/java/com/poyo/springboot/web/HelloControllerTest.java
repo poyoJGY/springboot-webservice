cdb2f5c1e801cf8d822060757bc84731dd634faa
jgy <duke407@naver.com>
2023-01-29 17:29:09 +0900

    update test code

--- src/test/java/com/poyo/springboot/web/HelloControllerTest.java
+++ src/test/java/com/poyo/springboot/web/HelloControllerTest.java
@@ -1,10 +1,14 @@
 package com.poyo.springboot.web;
 
+import com.poyo.springboot.config.auth.SecurityConfig;
 import com.poyo.springboot.web.dto.HelloResponseDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,12 +18,16 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     @Autowired  // 스프링이 관리하는 빈(Bean)을 주입 받는다.
     private MockMvc mvc;    // 스프링 MVC 테스트의 시작점
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -29,21 +37,21 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));    // mvc.perform의 결과 검증. 응답 본문의 내용 검증. Controller 에서 "hello"를 리턴하기 때문에 이 값이 맞는지 검증
     }
 
+    @WithMockUser(roles = "USER")
     @Test
-    public void 롬복_기능_테스트() throws Exception {
-
-        String name = "test";
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
         int amount = 1000;
 
         // param
         // API 테스트할 때 사용될 요청 파라미터를 설정
         // 값은 String만 허용
-        mvc.perform(get("/hello/dto")
-                .param("name", name)
-                .param("amount", String.valueOf(amount)))
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", is(name)))
                 .andExpect(jsonPath("$.amount", is(amount)));
-
     }
 }
